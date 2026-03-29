fa5429fafdaef8813676d3e7d7f54a98373e3d26
Krishuang-1116 <natsuno1116@icloud.com>
2026-03-29 14:05:31 +0200

    Implement data loading and average price retrieval
    Added functions to load benchmark data and calculate average price.

--- backend/services/scoring.py
+++ backend/services/scoring.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+def load_benchmark_data(file_path:str):
+  # This function is for loading data store inside /data
+  df = pd.read_csv(file_path)
+  return df
+  
+def get_avg_price(ville: str, df: pd.Dataframe) -> float:
+  # This function is for retrieving the avg price stored in the database 
+  # given the name of the city
+  result = df[df["Commune"] == ville]
+
+  # This is for preparing the case of input mismatch and user's ivalid input 
+  if result.empty:
+    raise ValueError(f"Invalid input for column city:{ville}")
+  return float(result["avg_price_m2"].values[0])
+
+  
+  
